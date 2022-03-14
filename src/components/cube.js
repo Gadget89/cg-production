b5d46099f598564c07292782021df7effa906e9f
Nicolas Roybal <nicolas.roybal@hotmail.com>
2022-03-14 17:41:49 -0500

    Fixed cube layout and buttons

--- src/components/cube.js
+++ src/components/cube.js
@@ -9,29 +9,16 @@ const Cube = ({ heading, subHeading, bttnText, anchor, img1, img2, img3 }) => (
     <div className="cube-container">
               <div className="photo-cube">
                 <div className="front photo-desc">
-                  {/* <Link to={ anchor }> */}
                     <h3 className="secondary-header">{ heading }</h3>
                     <img className="cube-img" src={img1} alt={subHeading} />
-                  {/* </Link> */}
                 </div>
                 <div className="back photo-desc">
                   <Link
                     to={ anchor }
-                    style={{
-                      border: 'none',
-                      borderImage: 'none',
-                      borderImageSlice: 'none'
-                    }}
+                    className="anchor-wrapper"
                   >
                     <h3 className="secondary-header">{ heading }</h3>
-                    <p
-                      style={{
-                        margin: '62px 10px 30px',
-                        minHeight: '88px',
-                        lineHeight: '1.25',
-                        color: 'white'
-                      }}
-                    >
+                    <p className="payload-text">
                       {subHeading}
                     </p>
 
